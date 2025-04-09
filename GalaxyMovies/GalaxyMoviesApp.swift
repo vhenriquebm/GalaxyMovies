a9fe9e5d447a3b2e024142604bcaf2d606c36a67
vhenriquebm <vhenriquebm@gmail.com>
2025-04-09 11:55:57 -0300

    Initial Commit

---
+++ GalaxyMovies/GalaxyMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalaxyMoviesApp.swift
+//  GalaxyMovies
+//
+//  Created by Vitor Henrique Barreiro Marinho on 09/04/25.
+//
+
+import SwiftUI
+
+@main
+struct GalaxyMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GalaxyMoviesTests/GalaxyMoviesTests.swift
@@ -0,0 +1,17 @@
+//
+//  GalaxyMoviesTests.swift
+//  GalaxyMoviesTests
+//
+//  Created by Vitor Henrique Barreiro Marinho on 09/04/25.
+//
+
+import Testing
+@testable import GalaxyMovies
+
+struct GalaxyMoviesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
